7777778bd02b54c60481cf3682cfefa42eade078
Tolik <tolik.daf.777@gmail.com>
2021-12-25 18:38:32 +0300

    Сделано GUI водителя и отправка сообщения для авто

--- src/tesla/ControlGUI.java
+++ src/tesla/ControlGUI.java
@@ -7,8 +7,11 @@ package tesla;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,6 +29,7 @@ public class ControlGUI extends JFrame {
     private JLabel brake;
     private final String res = "src/resources/";
     private ElonMuskAgent myAgent;
+
     
     public ControlGUI(ElonMuskAgent a){
         
@@ -33,30 +37,80 @@ public class ControlGUI extends JFrame {
         
         myAgent = a;
         
-        JPanel p_bottom = new JPanel(new BorderLayout());
+        setPreferredSize(new Dimension(250, 150));
         
-        JPanel control = new JPanel();
-        control.setLayout(new GridLayout(2,2));
+        JPanel p = new JPanel(new BorderLayout());
         
         left = new JLabel();
         left.setIcon(new ImageIcon(res + "red_l.png"));
-        control.add(left);
+        p.add(left,BorderLayout.WEST);
         
         right = new JLabel();
         right.setIcon(new ImageIcon(res + "red_r.png"));
-        control.add(right);
+        p.add(right,BorderLayout.EAST);
+        
+        JPanel control = new JPanel();
+        control.setLayout(new GridLayout(1,2));
         
         gas = new JLabel();
         gas.setText("Gas");
+        gas.setFont(new Font(gas.getName(),Font.PLAIN,20));
         gas.setForeground(Color.red);
         control.add(gas);
         
         brake = new JLabel();
         brake.setText("Brake");
+        brake.setFont(new Font(gas.getName(),Font.PLAIN,20));
         control.add(brake);
         
-        p_bottom.add(control,BorderLayout.CENTER);
+        p.add(control,BorderLayout.SOUTH);
+        
+        p.addKeyListener(new KeyListener(){
+            @Override
+            public void keyPressed(KeyEvent e){
+                switch(e.getExtendedKeyCode()){
+                    case(KeyEvent.VK_RIGHT):
+                        right.setIcon(new ImageIcon(res + "gr_r.png"));
+                        a.updateMes(1);
+                        break;
+                    case(KeyEvent.VK_LEFT):
+                        left.setIcon(new ImageIcon(res + "gr_l.png"));
+                        a.updateMes(2);
+                        break;
+                    case(KeyEvent.VK_UP):
+                        gas.setForeground(Color.red);
+                        brake.setForeground(Color.black);
+                        a.updateMes(3);
+                        break;
+                    case(KeyEvent.VK_DOWN):
+                        gas.setForeground(Color.black);
+                        brake.setForeground(Color.red);
+                        a.updateMes(4);
+                        break;
+                }
+            }
+            
+            @Override
+            public void keyReleased(KeyEvent e){
+                switch(e.getExtendedKeyCode()){
+                    case(KeyEvent.VK_RIGHT):
+                        right.setIcon(new ImageIcon(res + "red_r.png"));
+                    case(KeyEvent.VK_LEFT):
+                        left.setIcon(new ImageIcon(res + "red_l.png"));
+                }
+            }
+            
+            @Override
+            public void keyTyped(KeyEvent e){
+            }
+        });
+        p.setFocusable(true);
+        getContentPane().add(p, BorderLayout.CENTER);
+        
+        
     }
+    
+    
     
     public void showGui() {
          pack();
--- src/tesla/ElonMuskAgent.java
+++ src/tesla/ElonMuskAgent.java
@@ -1,7 +1,11 @@
 
 package tesla;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
 
 /**
  *
@@ -9,9 +13,35 @@ import jade.core.Agent;
  */
 public class ElonMuskAgent extends Agent {
     
-    private int d;
+    private int typeMove;
+    private final AID car = new AID("car", AID.ISLOCALNAME);
     
     protected void setup() {
+        ControlGUI gui = new ControlGUI(this);
+        gui.showGui();
+        typeMove = 4;
         
+        addBehaviour(new SendMoveMes(this,1000));
+    }
+    
+    public void updateMes(int typeMove){
+        this.typeMove =typeMove;
+    }
+    
+    private class SendMoveMes extends TickerBehaviour{
+
+        public SendMoveMes(Agent a, long period) {
+            super(a, period);
+        }
+        
+        @Override
+        protected void onTick() {
+            ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+            cfp.addReceiver(car);
+            cfp.setContent(String.valueOf(typeMove)); 
+            cfp.setConversationId("move"); 
+            cfp.setReplyWith("cfp" + System.currentTimeMillis()); 
+            myAgent.send(cfp); 
+        }
     }
 }
